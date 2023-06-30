4bc3f8e52a94398aab34b84a48a860d4d54035f7
Sourav01112 <chaudharysourav997@gmail.com>
2023-07-01 01:17:33 +0530

    Layout for Registration Done

--- Client/src/pages/Register/index.jsx
+++ Client/src/pages/Register/index.jsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { Button, Form, Input } from "antd";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import registerImage from "../../assets/register.jpg";
 
 export const Register = () => {
-  return <div>Register</div>;
+  const [type, setType] = useState("donor");
+
+  const location = useLocation();
+  const isDesiredRoute = location.pathname === "/register";
+  const backgroundClasses = isDesiredRoute ? "bg-wallpaper" : "";
+
+  return (
+    <div
+      className={`flex h-screen items-center justify-center bg-no-repeat bg-left bg-contain ${backgroundClasses}`}
+      style={{ backgroundImage: `url(${registerImage})` }}
+    >
+      <Form
+        layout="vertical"
+        className="bg-white rounded shadow grid grid-cols-2 p-5 gap-5 w-1/2"
+      >
+        <h1 className="col-span-2 uppercase text-2xl">
+          <span>Register : Donor </span>
+          <hr />
+        </h1>
+        <Form.Item label="Name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Email">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Phone">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Password">
+          <Input />
+        </Form.Item>
+        <Button type="primary" block className="col-span-2 uppercase bg-black">
+          Sign Up
+        </Button>
+        <Link to="/login" className="col-span-2 text-center text-gray-500">
+          Already have an account? Login
+        </Link>
+      </Form>
+    </div>
+  );
 };
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+const path = require("path");
+
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
