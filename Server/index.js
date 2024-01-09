2e401ba120b61fad63107b0f018f313606ce6db9
Sourav01112 <chaudharysourav997@gmail.com>
2024-01-10 04:30:51 +0530

    some deployment issue fix

--- Server/index.js
+++ Server/index.js
@@ -6,8 +6,9 @@ const { usersRouter } = require("./routes/user.routes");
 const { inventoryRouter } = require("./routes/inventory.routes");
 const Port = process.env.PORT || 4500;
 const ip = require('ip')
+const path = require("path");
 
-app.use(express.json()); //to access request.body variables
+app.use(express.json()); 
 app.use(cors());
 
 // User Routes
@@ -26,17 +27,16 @@ app.listen(Port, async () => {
     console.log("Mongo connection error");
   }
 });
+const isProduction = process.env.NODE_ENV === "production";
+const viteBuildDirectory = isProduction ? "Client/build" : "public";
 
-// deployment config
-const path = require("path");
-__dirname = path.resolve();
+// Serving static files
+app.use(express.static(path.join(process.cwd(), viteBuildDirectory)));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/Client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
-  });
-}
+// For all other routes, serve index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(process.cwd(), viteBuildDirectory, "index.html"));
+});
 
 
 console.log("app is running on ip " + ip.address())
